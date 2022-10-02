d4b5f330d2664b7bccade60d1605563e50bbcdfb
woshengchinima <89957827+zwangN@users.noreply.github.com>
2022-10-02 13:49:48 -0500

    1

---
+++ music_notato/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nicolewang/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nicolewang/Downloads/MusicNotato/music_notato"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/nicolewang/Downloads/MusicNotato/music_notato/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/nicolewang/Downloads/MusicNotato/music_notato/.dart_tool/package_config.json"
