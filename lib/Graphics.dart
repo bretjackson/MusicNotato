c1673d2d7fdd0352a51694b1a18946c87e734f3f
Olivia Laske <olaske@macalester.edu>
2022-10-27 21:34:33 -0500

    Updated code to match what Nicole had.

--- lib/Graphics.dart
+++ lib/Graphics.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -118,6 +119,65 @@ class Graphics extends CustomPainter {
     canvas.drawLine(Offset(0, x), Offset(size.width, x), paint);
     canvas.drawLine(Offset(0, 2*x), Offset(size.width, 2*x), paint);
 
+    var points = [
+      //上面横线
+      Offset(10, -2 * x - 5),
+      Offset(15, -2 * x - 5),
+      Offset(20, -2 * x - 5),
+      Offset(25, -2 * x - 5),
+      Offset(30, -2 * x - 5),
+      //左边竖线
+      Offset(5, -2 * x - 5),
+      Offset(5, -2 * x),
+      Offset(5, -2 * x + 5),
+      Offset(5, -2 * x + 10),
+      Offset(5, -2 * x + 15),
+      Offset(5, -2 * x + 20),
+      Offset(5, 2 * x - 20),
+      Offset(5, 2 * x - 15),
+      Offset(5, 2 * x - 5),
+      Offset(5, 2 * x),
+      Offset(5, 2 * x + 5),
+      //下面横线
+      Offset(10, 2 * x + 5),
+      Offset(15, 2 * x + 5),
+      Offset(20, 2 * x + 5),
+      Offset(25, 2 * x + 5),
+      Offset(30, 2 * x + 5),
+      //右边竖线
+      Offset(35, -2 * x - 5),
+      Offset(35, -2 * x),
+      Offset(35, -2 * x + 5),
+      Offset(35, -2 * x + 10),
+      Offset(35, -2 * x + 15),
+      Offset(35, -2 * x + 20),
+      Offset(35, 2 * x - 20),
+      Offset(35, 2 * x - 15),
+      Offset(35, 2 * x - 5),
+      Offset(35, 2 * x),
+      Offset(35, 2 * x + 5),
+    ];
+    //画节拍选择框
+    paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+    canvas.drawPoints(PointMode.points, points, paint);
+
+    //画节拍分子分母
+    var textPainter = TextPainter(
+        text: TextSpan(
+          text: '${signature}\n${signature_}',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center);
+    textPainter.layout();
+    textPainter.paint(canvas,
+        Offset(20 - (textPainter.width / 2), 0 - (textPainter.height / 2)));
+    
     for(int i = 0; i < noteList.length; i++) {
       Note currentNote = noteList[i];
       double xPosition = notePosition[i]; // x-coordinate of the note to be drawn
